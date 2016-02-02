f8cce47294daef39a8260ccca61eabfa58904536
Ivan Kriznar <ivan@simplified.me>
2016-02-02 16:40:27 +0100

    Lint mapping model method

--- src/ggrc/assets/javascripts/models/mappings.js
+++ src/ggrc/assets/javascripts/models/mappings.js
@@ -602,10 +602,10 @@
     },
     Audit: {
       _canonical: {
-        "requests": "Request",
-        "_program": "Program",
-        "context": "Context",
-        "related_objects_as_source": ["Control", "Assessment", "Issue"]
+        requests: 'Request',
+        _program: 'Program',
+        context: 'Context',
+        related_objects_as_source: ['Control', 'Assessment', 'Issue']
       },
       folders: Proxy(
         "ObjectFolder", "folderable", "folder",
