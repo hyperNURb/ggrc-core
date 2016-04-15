221579219d7527ca1dac8332caa0d3b8d8029fa3
Ivan Kriznar <ivan@simplified.me>
2016-04-15 17:46:17 +0200

    Generate custom attribute list only with filled values

--- src/ggrc/assets/javascripts/components/custom_attributes/custom_attributes.js
+++ src/ggrc/assets/javascripts/components/custom_attributes/custom_attributes.js
@@ -14,6 +14,38 @@
       // get loaded
       load: '@',
       loading: false,
+      caList: function () {
+        var instance = this.attr('instance');
+        var definitions = instance.custom_attribute_definitions || [];
+        var values = instance.custom_attribute_values || [];
+        var result = can.map(definitions, function (def) {
+          def = def.reify();
+          return can.map(values, function (ca) {
+            var response = {};
+            ca = ca.reify();
+
+            if (ca.custom_attribute_id !== def.id) {
+              return;
+            }
+
+            response.attribute_type = def.attribute_type;
+            response.title = def.title;
+            response.id = ca.id;
+
+            if (ca.attribute_value) {
+              response.attribute_value = ca.attribute_value;
+            }
+            if (ca.attribute_object) {
+              response.attribute_object = ca.attribute_object.reify();
+            }
+            if (response.attribute_value || response.attribute_object) {
+              return response;
+            }
+          });
+        });
+
+        return _.compact(_.flattenDeep(result));
+      },
       refreshAttributes: function () {
         this.attr('loading', true);
         $.when(
@@ -45,37 +77,6 @@
       }
       if (this.scope.load) {
         this.scope.refreshAttributes();
-      }
-    },
-    helpers: {
-      with_value_for_id: function (id, options) {
-        var ret;
-        id = Mustache.resolve(id);
-        can.each(this.instance.custom_attribute_values, function (value) {
-          value = value.reify();
-          if (value.custom_attribute_id === id) {
-            ret = value.attribute_value;
-          }
-        });
-        return options.fn(options.contexts.add({
-          value: ret
-        }));
-      },
-      with_object_for_id: function (id, options) {
-        var ret;
-        id = Mustache.resolve(id);
-        can.each(this.instance.custom_attribute_values, function (value) {
-          value = value.reify();
-          if (value.custom_attribute_id === id) {
-            ret = value.attribute_object;
-            if (ret) {
-              ret = ret.reify();
-            }
-          }
-        });
-        return options.fn(options.contexts.add({
-          object: ret
-        }));
       }
     }
   });
